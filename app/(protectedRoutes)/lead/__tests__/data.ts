3faec925c448e72cbff1488eaf24dcf565776fbe
Vivelune <shafiqueeee.atif@gmail.com>
2026-05-24 23:58:12 +0800

    5:Leads page and layout done along with mock data.

---
+++ app/(protectedRoutes)/lead/__tests__/data.ts
@@ -0,0 +1,21 @@
+export const leadData = [
+    {
+        name:'Isra',
+        email:'isra@gmail.com',
+        phone:'+1-123-1231-1234',
+        tags:['New Student', 'Coding'],
+    },
+    {
+        name:'Hassan',
+        email:'hassanconteh@gmail.com',
+        phone:'+1-123-1111-1231',
+        tags:['Project Building'],
+    },
+    {
+        name:'Alhaji',
+        email:'alhaji@gmail.com',
+        phone:'+09-123919293',
+        tags:['New Student', 'Project Building'],
+    },
+    
+]
